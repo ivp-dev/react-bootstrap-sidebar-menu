d207cb12fcef9890f928e7ee81131c21163d1bc7
Vadim I <ivp.dev@outlook.com>
2021-06-22 00:36:51 +0300

    * sidebar

--- src/sidebar-menu-navbar.tsx
+++ src/sidebar-menu-navbar.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Navbar } from "react-bootstrap";
-import { BsPrefixProps, BsPrefixRefForwardingComponent } from 'react-bootstrap/esm/helpers';
+import React, { useCallback, useMemo } from 'react';
+import { Navbar, NavbarProps } from "react-bootstrap";
+import { BsPrefixProps, BsPrefixRefForwardingComponent, SelectCallback } from 'react-bootstrap/esm/helpers';
 import PropTypes from "prop-types";
 import { useBootstrapPrefix } from 'react-bootstrap/esm/ThemeProvider';
 import { useUncontrolled } from 'uncontrollable';
+import SelectableContext from 'react-bootstrap/esm/SelectableContext';
+import NavbarContext, {NavbarContextType} from 'react-bootstrap/esm/NavbarContext';
+import classNames from 'classnames';
 
-type SidebarMenuNavbarProps = BsPrefixProps & Omit<React.HTMLAttributes<HTMLElement>, 'onSelect'> & {
+type SidebarMenuNavbarProps = BsPrefixProps & Omit<NavbarProps, 'sticky'> & {
   bg?: string
   onToggle?: (expanded: boolean) => void;
   expanded?: boolean;
@@ -45,8 +48,6 @@ const propTypes = {
   role: PropTypes.string
 };
 
-
-
 type SidebarMenuNavbar = BsPrefixRefForwardingComponent<'nav', SidebarMenuNavbarProps> & {
   Brand: typeof Navbar.Brand;
   Collapse: typeof Navbar.Collapse;
@@ -57,19 +58,51 @@ type SidebarMenuNavbar = BsPrefixRefForwardingComponent<'nav', SidebarMenuNavbar
 const SidebarMenuNavbar = React.forwardRef<HTMLElement, SidebarMenuNavbarProps>((props, ref) => {
   const {
     bsPrefix: initialBsPrefix,
+    expanded,
     as: Component = 'nav',
+    onSelect,
+    collapseOnSelect,
     onToggle,
+    className,
     bg,
     ...controlledProps 
   } = useUncontrolled(props, {
     expanded: 'onToggle',
   });
 
-  const bsPrefix = useBootstrapPrefix(initialBsPrefix, 'navbar');
+  const bsPrefix = useBootstrapPrefix(initialBsPrefix, 'sidebar-menu-navbar');
 
-  return <Component {...props} ref={ref}>
+  const navbarContext = useMemo<NavbarContextType>(
+    () => ({
+      onToggle: () => onToggle?.(!expanded),
+      bsPrefix,
+      expanded: !!expanded,
+    }),
+    [bsPrefix, expanded, onToggle],
+  );
 
-  </Component>;
+  const handleCollapse = useCallback<SelectCallback>(
+    (...args) => {
+      onSelect?.(...args);
+      if (collapseOnSelect && expanded) {
+        onToggle?.(false);
+      }
+    },
+    [onSelect, collapseOnSelect, expanded, onToggle],
+  );
+
+  return <NavbarContext.Provider value={navbarContext}>
+    <SelectableContext.Provider value={handleCollapse}>
+      <Component 
+        ref={ref} {...props}
+        {...controlledProps}
+        className={classNames(
+          className,
+          bsPrefix,
+          bg && `bg-${bg}`
+        )}/>
+    </SelectableContext.Provider>
+  </NavbarContext.Provider>;
 });
 
 //SidebarMenuNavbar.propTypes = propTypes;
