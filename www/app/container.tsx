77fede9c15ca1f57b1aa9231b66f83ae4c156364
ivp.net <ivp.net@outlook.com>
2021-06-23 18:16:40 +0300

    * sidebar style

--- www/app/container.tsx
+++ www/app/container.tsx
@@ -12,23 +12,12 @@ const MainLayoutComponent: React.FC<Props> = () => {
   return (
     <div className="main-wrapper">
       <SidebarMenu bg={bg} variant={bg}>
-        <SidebarMenu.Collapse getScrollValue={() => 240}>
+        <SidebarMenu.Collapse getScrollValue={() => 300}>
           <SidebarMenu.Header>
             <SidebarMenu.Brand>
-              <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap">
-                  <svg id="bootstrap" viewBox="0 0 118 94" width="100%" height="100%">
-                    <title>Bootstrap</title>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
-                  </svg></use></svg>
-                <span className="fs-4">Sidebar</span>
-              </a>
+              BOOTSTRAP-SIDEBAR
             </SidebarMenu.Brand>
-            <SidebarMenu.Toggle>
-              <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" className="octicon">
-                <path fillRule="evenodd" d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"></path>
-              </svg>
-            </SidebarMenu.Toggle>
+            <SidebarMenu.Toggle />
           </SidebarMenu.Header>
           <SidebarMenu.Navbar>
             <SidebarMenu.Nav>
@@ -139,14 +128,7 @@ const MainLayoutComponent: React.FC<Props> = () => {
         </SidebarMenu.Collapse>
       </SidebarMenu>
       <main className="main-container">
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-          <Tab eventKey="home" title="Home">
-            1
-          </Tab>
-          <Tab eventKey="profile" title="Profile">
-            2
-          </Tab>
-        </Tabs>
+
       </main>
       <footer className="main-footer"></footer>
     </div>
