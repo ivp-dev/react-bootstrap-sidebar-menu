70120732a9f769bb834cf0502d41ac830f058035
ivp.net <ivp.net@outlook.com>
2021-06-29 18:01:27 +0300

    * navlink

--- src/sidebar-menu-nav-link.tsx
+++ src/sidebar-menu-nav-link.tsx
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
 import { NavLink, NavLinkProps } from "react-bootstrap";
-import AbstractNavItem from 'react-bootstrap/AbstractNavItem' 
+import AbstractNavItem from 'react-bootstrap/AbstractNavItem'
 import { useBootstrapPrefix } from 'react-bootstrap/esm/ThemeProvider';
 import { BsPrefixRefForwardingComponent } from 'react-bootstrap/esm/helpers';
+import NavbarContext from 'react-bootstrap/NavbarContext';
+import NavContext from 'react-bootstrap/NavContext'
+import { makeEventKey } from 'react-bootstrap/SelectableContext'
 import classNames from 'classnames';
 
 type SidebarMenuNavLinkProps = NavLinkProps
@@ -12,9 +15,25 @@ const SidebarMenuNavLink: BsPrefixRefForwardingComponent<'a', SidebarMenuNavLink
   as: As = 'a',
   className,
   disabled,
+  active,
+  href,
   ...props
 }: SidebarMenuNavLinkProps, ref) => {
   const bsPrefix = useBootstrapPrefix(initialBsPrefix, 'sidebar-menu-nav-link');
+
+  const navContext = useContext(NavContext);
+  const navbarContext = useContext(NavbarContext);
+  const navKey = makeEventKey(href);
+
+  if (navContext) {
+    const isActive = active == null && navKey != null
+      ? navContext.activeKey === navKey
+      : active;
+
+    if (navbarContext && !navbarContext.expanded && isActive) {
+      console.log("should be expanded")
+    }
+  }
 
   return (
     <AbstractNavItem
